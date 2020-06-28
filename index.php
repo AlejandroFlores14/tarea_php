a21cac152b2015c8011db2105f7ec9b44f73ab77
AlejandroFlores14 <65797366+AlejandroFlores14@users.noreply.github.com>
2020-06-28 15:26:49 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" type="text/css" href="css/e2.css"/>
+</head>
+<body>
+	<header>
+	
+	<img src="img/logo.png" id="cabe"><h1 id="titu">Wallpapers</h1>
+		<nav id="menuP">
+
+               <ul >
+                   <li><a href= "index.php"><h3>Inicio</h3>  </a></li>
+                   
+                   <li><a href= "pares.php"><h3 >Pares </h3>  </a></li>
+                   
+                   <li><a href= "sueldo1.php"><h3>Aumento sueldo</h3>  </a></li>
+                   
+                   <li><a href= "AreaDeFigura1.php"><h3>Área Figura</h3> </a></li>
+                   
+                   <li><a href= "Carrera1.php"><h3>Carreras</h3> </a></li>
+
+                   <li><a href= "formaA1.php"><h3>Área de terreno</h3> </a></li>
+                   
+                   
+
+
+                   
+			   </ul> 
+		</nav>
+		
+	</header>
+
+	<center><h1>Indice</h1></center>
+	<br><br>
+	<center><a href= "pares.php"><h2>Impresion de números pares</h2> </a></center>
+		<br><br>
+		<center><a href= "sueldo1.php"><h2>Aumento de sueldo</h2> </a></center>
+		<br><br>
+		<center><a href= "AreaDeFigura1.php"><h2>Área de figura</h3> </a></center>
+		<br><br>
+		<center><a href= "Carrera1.php"><h2>Carreras Universitarias</h2> </a></center>
+		<br><br>
+		<center><a href= "formaA1.php"><h2>Área de terreno</h2> </a></center>
+		<br><br>
+		
+		
+
+
+
+  <footer id="foot"> 
+    <section id="izq">
+      <p id="top">Bernardo Alejandro Sánchez Flores</p>
+      <p>12/06/2020</p>
+      <p>Correo Electronico:familiaflores019@gmail.com</p>
+      
+    </section>
+  <section id="dere">
+      <p id="top"><img  src="img/alex.jpg" ></p>
+      
+      
+    </section>
+
+  </footer>
+		
+</body>
+</html>
